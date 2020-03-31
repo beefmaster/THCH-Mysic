4d842ed29b91136d2bb4e53b154c48994969b4ce
Cameron Baker <cameronbaker@192-168-1-14.tpgi.com.au>
2020-04-01 02:02:23 +1100

    css modifications and semi functional geolocation and ajax calls

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 function makefeed(posts){
     let root = document.getElementById("root");
 
@@ -12,14 +13,65 @@ function makefeed(posts){
     }
 }
 
-function makepost(data, eventJson){
-    let feed = document.getElementById("main");
+//if location found -> get country to find events in this case
+//as corona has shut most of them down
+function LocationFound(geolocation) {
+    console.log(geolocation.coords.latitude);
+    console.log(geolocation.coords.longitude)
+    var ObjList = [];
+    //should be modified to find country from geolocation
+    $.ajax({
+        url: 'https://app.ticketmaster.com/discovery/v2/events.json?size=10&countryCode=AU&apikey=faxAg8pOt9SAAoEkAz3mk53h67rADGAP',
+        dataTyp: 'JSON',
+        type: 'GET',
+        async: false,
+        cache: false,
+        sucess: function(data){
+            console.log(data);
+            console.log('ajax query sucess');
+            data = JSON.parse(data);
+            
+            //this issue is with the json parsing
+            //the json parsing is broken up into many lists and nest dictionaries
+            //need to be able to find each unqiue event to find the artist name
+            //and the venue
+            for(each in data){
+                var goodData =({location: each.venues, artist: each.name}); 
+                ObjList.push(goodData);
+            }   
+        },
+        error: function(xhr, status, error){
+            console.log("ajax query failed")
+            alert(status);
+        }
+    });
+    makepost(ObjList)
+    console.log("location finder finished")
+}
+
+function failedLocation(){
+    console.log('could not find location')
+}
+
+//data = {'location': latlng, 'artist': name}
+function makepost(data){
+    console.log("make post starting");
+    console.log(data)
+    let feed = document.getElementById("root");
 
     let newpost = document.createElement("li");
     newpost.setAttribute("class", "post");
-    newpost.innerHTML = data;
+    newpost.innerHTML = data.artist;
 
     feed.appendChild(newpost);
+
+    let postimage = document.createElement("img");
+    postimage.src = "harrystyle.jpg";
+    
+    newpost.appendChild(postimage);
+    
 }
 
-makefeed(["somedata", "data2", "bruh", "somedata", "data2", "bruh","somedata", "data2", "bruh","somedata", "data2", "bruh","somedata", "data2", "bruh","somedata", "data2", "bruh","somedata", "data2", "bruh","somedata", "data2", "bruh","somedata", "data2", "bruh","somedata", "data2", "bruh",])
+navigator.geolocation.getCurrentPosition(LocationFound, failedLocation);
+
+//#endregionmakefeed(["somedata", "data2", "bruh", "somedata", "data2", "bruh","somedata", "data2", "bruh","somedata", "data2", "bruh","somedata", "data2", "bruh","somedata", "data2", "bruh","somedata", "data2", "bruh","somedata", "data2", "bruh","somedata", "data2", "bruh","somedata", "data2", "bruh",])
