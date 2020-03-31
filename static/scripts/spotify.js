f5214127f8ee29027e62785613565341c40e734b
Thomas Sinn <z5213546@unsw.edu.au>
2020-04-01 01:28:28 +1100

    start js spotify

---
+++ static/scripts/spotify.js
@@ -0,0 +1,15 @@
+https://developer.spotify.com/documentation/web-api/reference/artists/get-artists-top-tracks/
+
+https://api.spotify.com/v1/search 
+
+var request = require("request");
+var token = "Bearer " 
+
+request({}
+
+
+
+
+
+
+)
